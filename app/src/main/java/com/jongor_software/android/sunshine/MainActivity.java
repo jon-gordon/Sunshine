dac39d5d71e8a7e7c6db1ebae09a96d7236a53bf
Jon Gordon <jon@jongor-software.com>
2015-02-16 23:42:05 +0000

    End of lesson 1

--- app/src/main/java/com/jongor_software/android/sunshine/MainActivity.java
+++ app/src/main/java/com/jongor_software/android/sunshine/MainActivity.java
@@ -1,15 +1,15 @@
 package com.jongor_software.android.sunshine;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -60,6 +60,27 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            String[] forecastArray = {
+                    "Today - Sunny - 88 / 63",
+                    "Tomorrow - Foggy - 70 / 46",
+                    "Weds - Cloudy - 72 / 63",
+                    "Thurs - Rainy - 64 / 51",
+                    "Fri - Foggy - 70 / 46",
+                    "Sat - Sunny - 76 / 68"
+            };
+
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                    getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textview,
+                    forecastArray
+            );
+
+            // We traverse "rootView" to search for our ListView (non-static, sub-tree, etc)
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(arrayAdapter);
+
             return rootView;
         }
     }
