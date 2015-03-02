a95817c5951e37e3efb268baaf875e7c48dbca14
Jon Gordon <jon@jongor-software.com>
2015-03-02 23:13:22 +0000

    Prettified list view

--- app/src/main/java/com/jongor_software/android/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/jongor_software/android/sunshine/ForecastAdapter.java
@@ -2,10 +2,11 @@ package com.jongor_software.android.sunshine;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.widget.CursorAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -42,14 +43,40 @@ public class ForecastAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View view = LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
-        return view;
+        return LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
         // Our view is simply a text view - keep UI functional with a simple (slow!) binding
-        TextView textView = (TextView)view;
-        textView.setText(convertCursorRowToUXFormat(cursor));
+
+        // Read weather icon from cursor
+        //int weatherId = cursor.getInt(ForecastFragment.COL_WEATHER_ID);
+        // Use placeholder for now
+        ImageView iconView = (ImageView) view.findViewById(R.id.list_item_icon);
+        iconView.setImageResource(R.drawable.ic_launcher);
+
+        // Read date from cursor
+        long date = cursor.getLong(ForecastFragment.COL_WEATHER_DATE);
+        TextView dateView = (TextView) view.findViewById(R.id.list_item_date_textview);
+        dateView.setText(Utility.getFriendlyDayString(context, date));
+
+        // Read weather forecast
+        String forecast = cursor.getString(ForecastFragment.COL_WEATHER_DESC);
+        TextView forecastView = (TextView) view.findViewById(R.id.list_item_forecast_textview);
+        forecastView.setText(forecast);
+
+        // Read user preference for metric/imperial
+        boolean isMetric = Utility.isMetric(context);
+
+        // Read high temperature from cursor
+        double high = cursor.getDouble(ForecastFragment.COL_WEATHER_MAX_TEMP);
+        TextView highView = (TextView) view.findViewById(R.id.list_item_high_textview);
+        highView.setText(Utility.formatTemperature(high, isMetric));
+
+        // Read low temperature from cursor
+        double low = cursor.getDouble(ForecastFragment.COL_WEATHER_MIN_TEMP);
+        TextView lowView = (TextView) view.findViewById(R.id.list_item_low_textview);
+        lowView.setText(Utility.formatTemperature(low, isMetric));
     }
 }
--- app/src/main/java/com/jongor_software/android/sunshine/Utility.java
+++ app/src/main/java/com/jongor_software/android/sunshine/Utility.java
@@ -3,14 +3,18 @@ package com.jongor_software.android.sunshine;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.text.format.Time;
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
-/**
- * Created by jon on 01/03/15.
- */
+
 public class Utility {
+
+    // Format used for storing dates in the database. Also used for converting those strings
+    // back into date objects for comparison/processing
+    public static final String DATE_FORMAT = "yyyyMMdd";
 
     public static String getPreferredLocation(Context context) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
@@ -38,5 +42,92 @@ public class Utility {
     public static String formatDate(long dateInMillis) {
         Date date = new Date(dateInMillis);
         return DateFormat.getDateInstance().format(date);
+    }
+
+    /**
+     * Helper method to convert the database representation of the date into something to display to
+     * users.  As classy and polished a user experience as "20150301" is, we can do better
+     *
+     * @param context Context to use for resource localisation
+     * @param dateInMillis The date in milliseconds
+     * @return A user-friendly representation of the date
+     */
+    public static String getFriendlyDayString(Context context, long dateInMillis) {
+        // The day string for forecast uses the following logic:
+        // For today: "Today, June 8"
+        // For tomorrow: "Tomorrow"
+        // For the next 5 days: "Wednesday" i.e. the day name
+        // For all days after that: "Mon Jun 8"
+
+        Time time = new Time();
+        time.setToNow();
+        long currentTime = System.currentTimeMillis();
+        int julianDay = Time.getJulianDay(dateInMillis, time.gmtoff);
+        int currentJulianDay = Time.getJulianDay(currentTime, time.gmtoff);
+
+        // If the date we're building the String for is today's date, the format is
+        // "Today, June 24"
+        if (julianDay == currentJulianDay) {
+            String today = context.getString(R.string.today);
+            int formatId = R.string.format_full_friendly_date;
+
+            return String.format(context.getString(
+                    formatId,
+                    today,
+                    getFormattedMonthDay(dateInMillis)));
+        }
+        else if (julianDay < currentJulianDay + 7) {
+            // If the input date is less than a week in the future, just return the day name
+            return getDayName(context, dateInMillis);
+        }
+        else {
+            // Otherwise, use the form "Mon Jun 3"
+            SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
+            return  shortenedDateFormat.format(dateInMillis);
+        }
+    }
+
+    /**
+     * Given a day, returns just the name to use for that day
+     * E.g. "today", "tomorrow", 'Wednesday"
+     *
+     * @param context Context to use for resource localisation
+     * @param dateInMillis The date in milliseconds
+     * @return Day representation in simple format
+     */
+    public static String getDayName(Context context, long dateInMillis) {
+        // If the date is today, return the localised version of "Today" instead of actual day name
+
+        Time time = new Time();
+        time.setToNow();
+        int julianDay = Time.getJulianDay(dateInMillis, time.gmtoff);
+        int currentJulianDay = Time.getJulianDay(System.currentTimeMillis(), time.gmtoff);
+
+        if (julianDay == currentJulianDay) {
+            return context.getString(R.string.today);
+        }
+        else if (julianDay == currentJulianDay + 1) {
+            return context.getString(R.string.tomorrow);
+        }
+        else {
+            // Otherwise, the format is just the day of the week (e.g "Wednesday".
+            SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE");
+            return dayFormat.format(dateInMillis);
+        }
+    }
+
+    /**
+     * Converts the db date format to the format "Month day", e.g "June 24"
+     *
+     * @param context Context to use for resource localisation
+     * @param dateInMillis The db formatted date string, expected to be of the form specified
+     *                     in Utility.DATE_FORMAT
+     * @return The day in the form of a string formatted "December 6"
+     */
+    public static String getFormattedMonthDay(long dateInMillis) {
+        Time time = new Time();
+        time.setToNow();
+        SimpleDateFormat monthDayFormat = new SimpleDateFormat("MMMM dd");
+        return monthDayFormat.format(dateInMillis);
     }
 }
